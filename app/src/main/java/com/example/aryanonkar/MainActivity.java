504990db069cac1c2928e373b29476140d0cdc5c
Aryan <aryanonkar2005@gmail.com>
2024-11-20 21:07:16 +0530

    Initial commit

--- app/src/main/java/com/example/aryanonkar/MainActivity.java
+++ app/src/main/java/com/example/aryanonkar/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.aryanonkar;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        View rootView = findViewById(android.R.id.content);
+        final View viewToHide = findViewById(R.id.chess_icon);
+
+        rootView.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            Rect r = new Rect();
+            rootView.getWindowVisibleDisplayFrame(r);
+            int screenHeight = rootView.getRootView().getHeight();
+            int keypadHeight = screenHeight - r.bottom;
+
+            if (keypadHeight > screenHeight * 0.15) {
+                // Keyboard is open
+                viewToHide.setVisibility(View.GONE);
+            } else {
+                // Keyboard is closed
+                viewToHide.setVisibility(View.VISIBLE);
+            }
+        });
+
+
     }
 }
