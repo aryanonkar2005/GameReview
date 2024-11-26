121b31f71d4dff18475ed834d92af9221afb0e97
Aryan <aryanonkar2005@gmail.com>
2024-11-26 11:27:40 +0530

    UI Finalized

--- app/src/main/java/com/example/aryanonkar/MainActivity.java
+++ app/src/main/java/com/example/aryanonkar/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.text.Editable;
-import android.text.NoCopySpan;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
